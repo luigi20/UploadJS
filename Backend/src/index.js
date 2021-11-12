4202dfe435e2ac03799eca17d2f3fe5ad476f7a2
Luis <luisopentec@gmail.com>
2021-11-12 15:07:59 -0300

    post route creation and basic upload of files to be burned to disk

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
 const routes = require('./routes');
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use(routes);
 
-app.listen(3333);
+app.listen(3000);
 
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const multer = require('multer');
 const routes = express.Router();
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => {
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
     return res.json({ Hello: 'world' });
 })
 
