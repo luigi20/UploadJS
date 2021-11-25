f5d0aa1b311df440c67dafabc9b8d76f576e789f
Luis <luisopentec@gmail.com>
2021-11-25 15:20:50 -0300

    frontend main screen and space to drag files to be uploaded

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import GlobalStyle from './styles/global';
+import { Container, Content } from './styles';
+import Upload from './components/upload';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Content><Upload /></Content>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/styles.js
@@ -0,0 +1,17 @@
+import style from 'styled-components';
+
+export const Container = style.div`
+    height:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+
+export const Content = style.div`
+    width:100%;
+    max-width:400px;
+    margin:30px;
+    background:#fff;
+    border-radius:4px;
+    padding:20px;
+`
